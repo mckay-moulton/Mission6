03af371f6d0a602b8dd76503f657323f8b1ca220
mckay <mckay@MCKAYSMACHINE>
2022-02-04 15:15:36 -0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Mission6.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission6.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult AddTask()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddTask(TaskForm task)
+        {
+            //when user submits the form, they will be taken to a confirmation page, and we can confirm the task they entered.
+            return View("Confirmation", task);
+        }
+
+        public IActionResult Confirmaton()
+        {
+            
+            return View();
+        }
+
+
+        public IActionResult ListView()
+        {
+            return View();
+        }
+        public IActionResult QuadView()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Models/TaskForm.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission6.Models
+{
+    public class TaskForm
+    {
+        public string TaskName { get; set; }
+        public DateTime DueDate { get; set; }   /*Not sure if datetime is the way to go, if it doesn't work please change*/
+        public int Quadrant { get; set; }
+        public string Category { get; set; }
+        public bool Completed { get; set; }
+
+    }
+}
